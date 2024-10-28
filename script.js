756d8fe0ab9b2b2b3f0397f65a5fcf74ef18aafe
trangtd26 <trangnguyenhai26@gmail.com>
2024-10-28 22:05:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openMenu() {
+    document.getElementById("side-bar").classList.add("open");
+    document.getElementById("menu-overlay").classList.add("active");
+}
+
+function closeMenu() {
+    document.getElementById("side-bar").classList.remove("open");
+    document.getElementById("menu-overlay").classList.remove("active");
+}
+
+//Xử lý hiệu ứng cuộn chuột
